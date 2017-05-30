69e17bb692553fe85116a5f1bb1129218cdda1e9
Alex Barry <aostreetart9@gmail.com>
2017-05-29 21:18:31 -0400

    Fixes

--- blendmocap.py
+++ blendmocap.py
@@ -4,6 +4,19 @@ Created on Mon May 29 03:54:30 2017
 
 Motion Capture Addon
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 @author: alex
 """
 
@@ -24,15 +37,28 @@ class CopyBoneRotations(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     source = bpy.props.StringProperty(name="SourceObject", default="SourceObject")
     destination = bpy.props.StringProperty(name="DestinationObject", default="DestinationObject")
-    
+
+    # Called when operator is run
     def execute(self, context):
+
+        # Get the logger
+        print("Copying Bone Rotation from %s to %s" % (self.source, self.destination))
         
-        # Called by blender when the addon is run
-        rolls = {}        
-        
+        # Setup
+        rolls = {}
+        # Deselect everything
+        for ob in bpy.context.selected_objects:
+            ob.select = False
+
         # Get the source and target armatures
         src = bpy.context.scene.objects[self.source]
         trg = bpy.context.scene.objects[self.destination]
+
+        # Select the source and target armatures
+        src.select = True
+        trg.select = True
+
+        print("Pulling Bone Roll from Source")
 
         # Pull the Roll values from the source armature        
         bpy.context.scene.objects.active = src
@@ -40,7 +66,9 @@ class CopyBoneRotations(bpy.types.Operator):
         for eb in src.data.edit_bones:
             rolls[eb.name] = eb.roll
             bpy.ops.object.mode_set(mode='POSE')
-        
+
+        print("Writing Bone Roll to Target")
+
         # Place the Roll values in the target armature
         bpy.context.scene.objects.active = trg
         bpy.ops.object.mode_set(mode='EDIT')
