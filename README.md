bec340643b998a56f1615bdcf19f9593683159c8
Alex Barry <aostreetart9@gmail.com>
2017-05-31 01:54:00 -0400

    Design added and initial operators moved in line with design

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # BlendMoCap by AO
 
-BlendMoCap is a Blender Addon designed to allow the use of BVH data in real animations.  This project was started due to the out-of-date addon that ships with blender, which doesn't appear to have worked correctly since about 2005.
+BlendMoCap is a Blender Addon designed to allow the use of BVH data in real animations.  This project was started due to the out-of-date addon that ships with blender, which doesn't appear to have worked correctly for some time.
 
 This project is currently under active developement and should not be considered stable.  Check back for more updates.
 
 ## Use
 
 Simply download the .py file, go to User Preferences in Blender, go to the Addons tab, and 'Install from File'.
-
--- blendmocap.py
+++ blendmocap.py
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 bl_info = {
-    "name": "AOMotionCapture", 
+    "name": "BlendMoCap",
     "author": "Alex Barry",
     "version": (0, 0, 1),
     "blender": (2, 78, 0),
@@ -32,98 +32,157 @@ bl_info = {
 import bpy
 import time
 
+# TO-DO: Transform the mocap constraints to keyframes
+def createMocapKeyframes(src, trg, keyframe_interval):
+    print("Creating Motion Capture Keyframes on %s with target %s" % (src.name, trg.name))
+
+# Select an object and switch to pose mode
+def selectAndPose(src):
+    # Deselect everything
+    for ob in bpy.context.selected_objects:
+        ob.select = False
+
+    # Select the source armature
+    src.select = True
+    bpy.context.scene.objects.active = src
+
+    # Set pose mode and select all the bones in the armature
+    bpy.ops.object.mode_set(mode='POSE')
+
+# Removes the mocap constraints
+def removeMocapConstraints(src):
+    print("Removing Motion Capture Constraints from %s" % (src.name))
+
+    selectAndPose(src)
+    bpy.ops.pose.select_all(action='SELECT')
+
+    # iterate over the pose bones
+    for bone in bpy.context.selected_pose_bones:
+        for c in bone.constraints:
+            if c.type == 'COPY_ROTATION' or c.type == 'COPY_LOCATION':
+                bone.constraints.remove(c)
+
+# Copy the roll values for all bones in one armature to another
+# TO-DO: Only map to the target bones that were taken from the source
+def copyBoneRolls(src, trg):
+    # Setup
+    rolls = {}
+
+    print("Pulling Bone Roll from Source")
+
+    # Pull the Roll values from the source armature
+    bpy.context.scene.objects.active = src
+    bpy.ops.object.mode_set(mode='EDIT')
+    for eb in src.data.edit_bones:
+        rolls[eb.name] = eb.roll
+        bpy.ops.object.mode_set(mode='POSE')
+
+    print("Writing Bone Roll to Target")
+
+    # Place the Roll values in the target armature
+    bpy.context.scene.objects.active = trg
+    bpy.ops.object.mode_set(mode='EDIT')
+    for eb in trg.data.edit_bones:
+        oldRoll = eb.roll
+        eb.roll = rolls[eb.name]
+        print(eb.name, oldRoll, eb.roll)
+
+# Setup Copy Rotation & Location Constraints
+def addMocapConstraints(src, trg):
+    print("Adding Location Constraints to %s with target %s" % (src.name, trg.name))
+
+    # iterate over the pose bones
+    for bone in bpy.context.selected_pose_bones:
+
+        # Apply a Copy Rotation Constraint to each pose bone
+        new_constraint = bone.constraints.new('COPY_LOCATION')
+
+        # Set up the constraint target and set it to copy pose data
+        new_constraint.target = bpy.data.objects[self.trg]
+        new_constraint.subtarget = bone.name
+        new_constraint.target_space = 'POSE'
+        new_constraint.owner_space = 'POSE'
+
+    # Turn off pose mode so that we can do basic operations
+    bpy.ops.object.posemode_toggle()
+
+    print("Adding Rotation Constraints to %s with target %s" % (self.constrained, self.target))
+
+    selectAndPose(src)
+    bpy.ops.pose.select_all(action='SELECT')
+
+    # iterate over the pose bones
+    for bone in bpy.context.selected_pose_bones:
+
+        # Apply a Copy Rotation Constraint to each pose bone
+        new_constraint = bone.constraints.new('COPY_ROTATION')
+
+        # Set up the constraint target and set it to copy pose data
+        new_constraint.target = bpy.data.objects[self.target]
+        new_constraint.subtarget = bone.name
+        new_constraint.target_space = 'POSE'
+        new_constraint.owner_space = 'POSE'
+
 class CopyBoneRotations(bpy.types.Operator):
     bl_idname = "object.copy_bone_roll"
     bl_label = "Copy Bone Roll"
     bl_options = {'REGISTER', 'UNDO'}
-    source = bpy.props.StringProperty(name="SourceObject", default="SourceObject")
-    destination = bpy.props.StringProperty(name="DestinationObject", default="DestinationObject")
 
     # Called when operator is run
     def execute(self, context):
 
-        print("Copying Bone Rotation from %s to %s" % (self.source, self.destination))
-        
-        # Setup
-        rolls = {}
-        # Deselect everything
+        # Find the source and target armatures
+        trg = bpy.context.scene.objects.active
+        src = None
         for ob in bpy.context.selected_objects:
-            ob.select = False
+            if ob.name != src.name:
+                src = ob
 
-        # Get the source and target armatures
-        src = bpy.context.scene.objects[self.source]
-        trg = bpy.context.scene.objects[self.destination]
+        if src is None:
+            print("No source selected")
+        else:
+            print("Copying Bone Rotation from %s to %s" % (src.name, trg.name))
+            copyBoneRolls(src, trg)
 
-        # Select the source and target armatures
-        src.select = True
-        trg.select = True
-
-        print("Pulling Bone Roll from Source")
-
-        # Pull the Roll values from the source armature        
-        bpy.context.scene.objects.active = src
-        bpy.ops.object.mode_set(mode='EDIT')
-        for eb in src.data.edit_bones:
-            rolls[eb.name] = eb.roll
-            bpy.ops.object.mode_set(mode='POSE')
-
-        print("Writing Bone Roll to Target")
-
-        # Place the Roll values in the target armature
-        bpy.context.scene.objects.active = trg
-        bpy.ops.object.mode_set(mode='EDIT')
-        for eb in trg.data.edit_bones:
-            oldRoll = eb.roll
-            eb.roll = rolls[eb.name]
-            print(eb.name, oldRoll, eb.roll)
-        
         #Let's blender know the operator is finished
         return {'FINISHED'}
-        
-    def invoke(self, context, event):
-        wm = context.window_manager
-        return wm.invoke_props_dialog(self)
 
+# Blender Operators
+# Functions exposed via the Blender UI
 
-class AddRotationConstraints(bpy.types.Operator):
-    bl_idname = "object.add_rotation_constraints"
-    bl_label = "Add Rotation Constraints"
+class TransferMoCapData(bpy.types.Operator):
+    bl_idname = "object.transfer_mocap_data"
+    bl_label = "Transfer MoCap Data"
     bl_options = {'REGISTER', 'UNDO'}
-    constrained = bpy.props.StringProperty(name="ConstrainedObject", default="ConstrainedObject")
-    target = bpy.props.StringProperty(name="TargetObject", default="TargetObject")
+    generate_constraints = bpy.props.BoolProperty(name="GenerateConstraints")
+    generate_keyframes = bpy.props.BoolProperty(name="GenerateKeyframes")
+    keyframe_interval = bpy.props.IntProperty(name="KeyFrameInterval", default="KeyFrameInterval")
 
     # Called when operator is run
     def execute(self, context):
 
-        print("Adding Rotation Constraints to %s with target %s" % (self.constrained, self.target))
+        interv = self.keyframe_interval
+        gen_kf = self.generate_keyframes
+        gen_cn = self.generate_constraints
 
-        # Deselect everything
+        # Find the source and target armatures
+        trg = bpy.context.scene.objects.active
+        src = None
         for ob in bpy.context.selected_objects:
-            ob.select = False
+            if ob.name != src.name:
+                src = ob
 
-        # Get the source and target armatures
-        src = bpy.context.scene.objects[self.constrained]
-        trg = bpy.context.scene.objects[self.target]
-
-        # Select the source armature
-        src.select = True
-        bpy.context.scene.objects.active = src
-
-        # Set pose mode and select all the bones in the armature
-        bpy.ops.object.mode_set(mode='POSE')
-        bpy.ops.pose.select_all(action='SELECT')
-
-        # iterate over the pose bones
-        for bone in bpy.context.selected_pose_bones:
-
-            # Apply a Copy Rotation Constraint to each pose bone
-            new_constraint = bone.constraints.new('COPY_ROTATION')
-
-            # Set up the constraint target and set it to copy pose data
-            new_constraint.target = bpy.data.objects[self.target]
-            new_constraint.subtarget = bone.name
-            new_constraint.target_space = 'POSE'
-            new_constraint.owner_space = 'POSE'
+        if src is None:
+            print("no source selected")
+        else:
+            if (gen_cn):
+                # Set up the Motion Capture Constraints
+                addMocapConstraints(src, trg)
+            if (gen_kf):
+                # Transform the constraints to keyframes
+                createMocapKeyframes(src, trg, interv)
+                # Remove the Motion Capture Constraints
+                removeMocapConstraints(src)
 
         # Let's blender know the operator is finished
         return {'FINISHED'}
@@ -132,54 +191,32 @@ class AddRotationConstraints(bpy.types.Operator):
         wm = context.window_manager
         return wm.invoke_props_dialog(self)
 
-class AddLocationConstraints(bpy.types.Operator):
-    bl_idname = "object.add_location_constraints"
-    bl_label = "Add Location Constraints"
+class CopyArmaturewithoutAnimation(bpy.types.Operator):
+    bl_idname = "object.copy_armature_without_animation"
+    bl_label = "Copy Armature without Animation"
     bl_options = {'REGISTER', 'UNDO'}
-    constrained = bpy.props.StringProperty(name="ConstrainedObject", default="ConstrainedObject")
-    target = bpy.props.StringProperty(name="TargetObject", default="TargetObject")
 
     # Called when operator is run
     def execute(self, context):
 
-        print("Adding Location Constraints to %s with target %s" % (self.constrained, self.target))
+        # Duplicate the armature
+        bpy.ops.object.duplicate_move()
 
-        # iterate over the pose bones
-        for bone in bpy.context.selected_pose_bones:
-
-            # Apply a Copy Rotation Constraint to each pose bone
-            new_constraint = bone.constraints.new('COPY_LOCATION')
-
-            # Set up the constraint target and set it to copy pose data
-            new_constraint.target = bpy.data.objects[self.target]
-            new_constraint.subtarget = bone.name
-            new_constraint.target_space = 'POSE'
-            new_constraint.owner_space = 'POSE'
+        # Remove the keyframes from the armature
+        bpy.ops.anim.keyframe_clear_v3d()
 
         # Let's blender know the operator is finished
         return {'FINISHED'}
 
-    def invoke(self, context, event):
-        wm = context.window_manager
-        return wm.invoke_props_dialog(self)
-
-def copy_bone_rot_menu_func(self, context):
-    self.layout.operator(CopyBoneRotations.bl_idname)
-
-def add_rot_constraint_menu_func(self, context):
-    self.layout.operator(AddRotationConstraints.bl_idname)
-        
 def register():
     bpy.utils.register_class(CopyBoneRotations)
-    bpy.utils.register_class(AddRotationConstraints)
-    bpy.types.VIEW3D_MT_object.append(copy_bone_rot_menu_func)
-    bpy.types.VIEW3D_MT_object.append(add_rot_constraint_menu_func)
-    
+    bpy.utils.register_class(CopyArmaturewithoutAnimation)
+    bpy.utils.register_class(TransferMoCapData)
+
 def unregister():
-    bpy.types.VIEW3D_MT_object.remove(add_rot_constraint_menu_func)
-    bpy.types.VIEW3D_MT_object.remove(copy_bone_rot_menu_func)
-    bpy.utils.unregister_class(AddRotationConstraints)
+    bpy.utils.unregister_class(TransferMoCapData)
+    bpy.utils.unregister_class(CopyArmaturewithoutAnimation)
     bpy.utils.unregister_class(CopyBoneRotations)
-    
+
 if __name__ == "__main__":
     register()
